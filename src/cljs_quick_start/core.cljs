b0121b636c4e040cdd6c7cb431fdf77eff26a5fd
Antônio <a.sousa.jnri@gmail.com>
2023-12-29 15:33:13 -0300

    Update react package

--- src/cljs_quick_start/core.cljs
+++ src/cljs_quick_start/core.cljs
@@ -1,14 +1,11 @@
-(ns cljs-quick-start.core)
-  ; (:require react-dom))
-
-; (println "Hello, World!")
-
-; (.render js/ReactDOM
-;   (.createElement js/React "h2" nil "Hello, React!")
-;   (.getElementById js/document "app"))
+(ns cljs-quick-start.core
+  (:require react-dom))
 
 (defn after-load! []
   (js/console.log "after load!"))
 
 (defn main! []
-  (js/console.log "main!"))
+  (js/console.log "main!")
+  (.render js/ReactDOM
+    (.createElement js/React "h2" nil "Hello, React!")
+    (.getElementById js/document "app")))
